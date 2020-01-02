e5772d27024f30bd8683609aed052094b9b8c742
danieljbingham <djm.bingham@btinternet.com>
2020-01-02 22:58:45 +0000

    Old December version Doesn't work anymore (AFAIK)

--- run.user.js
+++ run.user.js
@@ -1,99 +1,135 @@
 // ==UserScript==
 // @name         Google image sizer
 // @namespace    https://github.com/danieljbingham/google-image-sizer
-// @version      1.0.3.1
+// @version      1.0.3.3
 // @description  re-implement Google Images size filter
 // @author       Daniel Bingham
 // @include      http*://*.google.tld/search*tbm=isch*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
 
-var sz= /[?&]tbs=(?:[^&]+,)?isz:([^&,]+)/.test(location.search) && RegExp.$1;
+	var sz = /[?&]tbs=(?:[^&]+,)?isz:([^&,]+)/.test(location.search) && RegExp.$1;
+	var sizes = [["isz_2", "2mp"], ["isz_4", "4mp"], ["isz_6", "6mp"], ["isz_8", "8mp"], ["isz_10", "10mp"],
+	["isz_12", "12mp"], ["isz_15", "15mp"], ["isz_20", "20mp"], ["isz_40", "40mp"], ["isz_70", "70mp"]];
 
-/*
-Iterate through list of image sizes and add them to the DOM
-*/
-function addSizes() {
-	var icon = document.getElementById("isz_i").parentNode.querySelector(':scope * + .hdtbItm:not([tabindex])');
-	var parent = icon.parentNode;
-	var sizes = [["isz_2", "2mp"],["isz_4", "4mp"],["isz_6", "6mp"],["isz_8", "8mp"],["isz_10", "10mp"],
-		["isz_12", "12mp"],["isz_15", "15mp"],["isz_20", "20mp"],["isz_40", "40mp"],["isz_70", "70mp"]];
-	var clone, child, chkAny=false;
-	for (var i=0; i < sizes.length; i++) {
-		clone = icon.cloneNode(true);
-		clone.id = sizes[i][0];
-		if (sizes[i][1]==sz) {
-			clone.classList.add('hdtbSel');
-			child=clone;
-			chkAny=true;
-			parent.previousSibling.querySelector('.mn-hd-txt').textContent = "Larger than " + sizes[i][1].toUpperCase();
-			parent.previousSibling.classList.add('hdtb-tsel');
-			}
+	/*
+	Add sizes for one of two possibilities for the Google Images layout
+	*/
+	function addSizes() {
+		if (document.getElementById('isz_i')) {
+			addSizesIsz();
+		}
 		else {
-			child = clone.firstChild;
-			child.href = child.href.replace(/(tbs=(?:[^&]+,)?)isz(:[^&,]+)/, "$1"+"isz:" + sizes[i][1]);
+			addSizesAria();
+		}
+	}
+
+	/*
+	Iterate through list of image sizes and add them to the DOM (when 'isz' elements are present)
+	*/
+	function addSizesIsz() {
+		var icon = document.getElementById("isz_i").parentNode.querySelector(':scope * + .hdtbItm:not([tabindex])');
+		var parent = icon.parentNode;
+		var clone, child, chkAny = false;
+		for (var i = 0; i < sizes.length; i++) {
+			clone = icon.cloneNode(true);
+			clone.id = sizes[i][0];
+			if (sizes[i][1] == sz) {
+				clone.classList.add('hdtbSel');
+				child = clone;
+				chkAny = true;
+				parent.previousSibling.querySelector('.mn-hd-txt').textContent = "Larger than " + sizes[i][1].toUpperCase();
+				parent.previousSibling.classList.add('hdtb-tsel');
 			}
-		child.textContent = "Larger than " + sizes[i][1].toUpperCase();
+			else {
+				child = clone.firstChild;
+				child.href = child.href.replace(/(tbs=(?:[^&]+,)?)isz(:[^&,]+)/, "$1" + "isz:" + sizes[i][1]);
+			}
+			child.textContent = "Larger than " + sizes[i][1].toUpperCase();
 
-		parent.appendChild(clone);
+			parent.appendChild(clone);
+		}
+
+		var fc = parent.firstElementChild;
+		if (chkAny && fc.classList.contains('hdtbSel')) {
+			fc.classList.remove('hdtbSel');
+			child = icon.firstChild.cloneNode(true);
+			child.href = child.href.replace(/(tbs=(?:[^&]+,)?)isz(:[^&,]+)/, "$1");
+			child.textContent = fc.textContent;
+			fc.textContent = '';
+			fc.appendChild(child);
+		}
 	}
 
-	var fc = parent.firstElementChild;
-	if (chkAny && fc.classList.contains('hdtbSel')) {
-		fc.classList.remove('hdtbSel');
-		child=icon.firstChild.cloneNode(true);
-		child.href= child.href.replace(/(tbs=(?:[^&]+,)?)isz(:[^&,]+)/, "$1");
-		child.textContent = fc.textContent;
-		fc.textContent='';
-		fc.appendChild(child);
+	/*
+	Iterate through list of image sizes and add them to the DOM (when aria-labels are used)
+	*/
+	function addSizesAria() {
+		var icon = document.querySelectorAll('[aria-label="Icon"]').item(0);
+		var parent = icon.parentNode;
+
+		var clone = false;
+		for (var i = 0; i < sizes.length; i++) {
+			clone = icon.cloneNode(true);
+			clone.href = clone.href.replace(/(tbs=(?:[^&]+,)?)isz(%3A[^&,]+)/, "$1" + "isz:lt,islt:" + sizes[i][1]);
+			clone.firstChild.firstChild.textContent = "Larger than " + sizes[i][1].toUpperCase();
+			parent.appendChild(clone);
 		}
-}
-
-/*
-Check the user is on google images
-*/
-function isImageUrl() {
-	return location.href.includes("tbm=isch");
-}
-
-/*
-Check sizes not there already
-*/
-function needSizes() {
-	return document.getElementById("isz_i").parentNode.childNodes.length < 6
-}
-
-/*
-Show sizes/dimensions by default instead of on hover
-*/
-function showSizesDefault() {
-	var css = ".rg_anbg {display: none !important;} .rg_l:hover .rg_anbg {display: block !important;} " +
-		".rg_ilmbg {display: block !important;} .rg_l:hover .rg_ilmbg {display: none !important;}";
-	var style = document.createElement('style');
-
-	if (style.styleSheet) {
-		style.styleSheet.cssText = css;
-	} else {
-		style.appendChild(document.createTextNode(css));
 	}
 
-	document.getElementsByTagName('head')[0].appendChild(style);
-}
+	/*
+	Check the user is on google images
+	*/
+	function isImageUrl() {
+		return location.href.includes("tbm=isch");
+	}
 
-//run script
-showSizesDefault();
-
-var maxTries=100;
-function chk() {
-	if (!document.getElementById('isz_i')) {
-		if (maxTries--) setTimeout(chk,100);
-		return;
+	/*
+	Check sizes not there already
+	*/
+	function needSizes() {
+		var nodesLoggedOut = document.querySelectorAll('[aria-label="Large"]');
+		if (nodesLoggedOut.length != 0) {
+			return nodesLoggedOut.item(0).parentNode.childNodes.length < 6;
 		}
-	if (needSizes()) addSizes();
-}
+		else {
+			return document.getElementById("isz_i").parentNode.childNodes.length < 6
+		}
+	}
 
-if (isImageUrl()) chk();
+	/*
+	Show sizes/dimensions by default instead of on hover
+	*/
+	function showSizesDefault() {
+		var css = ".rg_anbg {display: none !important;} .rg_l:hover .rg_anbg {display: block !important;} " +
+			".rg_ilmbg {display: block !important;} .rg_l:hover .rg_ilmbg {display: none !important;}";
+		var style = document.createElement('style');
+
+		if (style.styleSheet) {
+			style.styleSheet.cssText = css;
+		} else {
+			style.appendChild(document.createTextNode(css));
+		}
+
+		document.getElementsByTagName('head')[0].appendChild(style);
+	}
+
+	//run script
+	showSizesDefault();
+
+	//somewhere: document.querySelectorAll('[role="listitem"]').item(0).addEventListener()
+
+	var maxTries = 100;
+	function chk() {
+		if (!document.getElementById('isz_i') && (document.querySelectorAll('[aria-label="Large"]').length == 0)) {
+			if (maxTries--) setTimeout(chk, 100);
+			return;
+		}
+		if (needSizes()) addSizes();
+	}
+
+	if (isImageUrl()) chk();
 
 })();
